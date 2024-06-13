08f3ec66d14c0d07775225aba91f270492c66c35
zhihaofans <zhihaofans@hotmail.com>
2024-06-13 23:53:34 +0800

    Initial Commit

---
+++ bilibili/bilibiliApp.swift
@@ -0,0 +1,17 @@
+//
+//  bilibiliApp.swift
+//  bilibili
+//
+//  Created by zzh on 2024/6/13.
+//
+
+import SwiftUI
+
+@main
+struct bilibiliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
