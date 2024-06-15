bfc906d63447a80b830b353a2fdbdc4be9b180ec
zhihaofans <zhihaofans@hotmail.com>
2024-06-15 23:52:22 +0800

    Add StringEx.swift

---
+++ bilibili/SwiftEx/StringEx.swift
@@ -0,0 +1,14 @@
+//
+//  StringEx.swift
+//  bilibili
+//
+//  Created by zzh on 2024/6/14.
+//
+
+import Foundation
+
+extension String {
+    var isNotEmpty: Bool {
+        return !self.isEmpty
+    }
+}
