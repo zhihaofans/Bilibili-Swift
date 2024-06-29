60341802bc6f0956ad56bbaf8a5bba95ff5ca34a
zhihaofans <zhihaofans@hotmail.com>
2024-06-30 00:25:09 +0800

    新增设置页面，查看登录数据

---
+++ bilibili/Views/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  bilibili
+//
+//  Created by zzh on 2024/6/29.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
